8ed648fbc47078d7803e40f4b40fd41801cda44b
theEndless11 <aaa380144@gmail.com>
2025-02-05 15:05:54 +0530

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const pool = require('../utils//db');
+const pool = require('../utils/db');
 
-const router = express.Router();
-
-// Get all users, excluding the current user
-router.get('/', (req, res) => {
+module.exports = async (req, res) => {
   const userId = req.query.userId;
 
-  pool.query('SELECT id, username FROM users WHERE id != ?', [userId], (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: 'Server error' });
-    }
+  if (!userId) {
+    return res.status(400).json({ message: 'userId is required' });
+  }
+
+  try {
+    const [results] = await pool.query('SELECT id, username FROM users WHERE id != ?', [userId]);
 
     res.status(200).json({ users: results });
-  });
-});
-
-module.exports = router;
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 

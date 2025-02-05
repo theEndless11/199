4a6ff9a2f0c9de572ea7ce3cb60acacb6a763a7d
theEndless11 <aaa380144@gmail.com>
2025-02-05 12:55:41 +0530

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const pool = require('../utils/db');
-
 const router = express.Router();
 
 // Get a specific user by ID (for the logged-in user)
@@ -20,18 +19,4 @@ router.get('/:userId', (req, res) => {
   });
 });
 
-// Get all users excluding the current logged-in user
-router.get('/', (req, res) => {
-  const userId = req.query.userId; // Get the logged-in user's ID from the query parameter
-
-  pool.query('SELECT id, username FROM users WHERE id != ?', [userId], (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: 'Server error' });
-    }
-
-    res.status(200).json({ users: results });
-  });
-});
-
 module.exports = router;
-
